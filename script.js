9c914d0d78d7cf19c48387bfcf554720a8946e3e
Viacheslav <moskalchukvs@gmail.com>
2020-02-29 16:12:39 +0300

    start_point

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+//a = 5;
+//console.log(a);
+
+//var leftBorderWidth = 1;
+//let second = 2;
+//const pi = 3.14;
+//alert(leftBorderWidth);
+// console.log(second);
+//console.log(4/0);
+//console.log('string'*9);
+//var sym = Symbol();
+//var obj = {};
+//console.log(obj);
+//console.log(sym);
+
+/*let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}
+*/
+
+//console.log(person['name']);
+
+//let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+
+//console.log(arr[0]);
+
+//alert('hello world');
+let answer = +prompt("Are you 18 years old?", "Yes");
+
+/*
+console.log(typeof(answer));
+console.log('arr' + 'ret');
+console.log(4 + +"- object");
+*/
+
+let incr = 10;
+    decr = 10;
+
+//incr++;
+//decr--;
+
+    console.log(incr++);
+    console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2); 
+console.log("2" === 2); 
+
+let isChecked = true;
+    isClose = false;
+
+console.log(isChecked || !isClose);
